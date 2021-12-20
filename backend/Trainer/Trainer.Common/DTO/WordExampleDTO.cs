aab328205a1d25397b34c3212ff69fb0ffed5c05
sleepergoose <sleepergoose@gmail.com>
2021-12-20 14:58:09 +0200

    Added endpoint api/WordExamples

---
+++ backend/Trainer/Trainer.Common/DTO/WordExampleDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Trainer.Common.DTO
+{
+    public sealed class WordExampleDTO
+    {
+        public string Phrase { get; set; }
+        public string Translation { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+    }
+}
