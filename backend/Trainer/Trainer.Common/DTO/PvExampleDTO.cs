22daa82c77b56d517ba1ac7b38c75a668fc0c6da
sleepergoose <sleepergoose@gmail.com>
2021-12-20 15:51:06 +0200

    Added endpoint api/PvExamples

---
+++ backend/Trainer/Trainer.Common/DTO/PvExampleDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trainer.Common.DTO
+{
+    public sealed class PvExampleDTO
+    {
+        public string Phrase { get; set; }
+        public string Translation { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+    }
+}
