a75913b5a10a7022614aa9d323760108b53f15ec
sleepergoose <sleepergoose@gmail.com>
2021-12-20 12:09:52 +0200

    Added endpoint api/Words

---
+++ backend/Trainer/Trainer.BL/Services/WordsService.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Trainer.DAL.Context;
+using Trainer.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Trainer.Common.DTO;
+
+namespace Trainer.BL.Services
+{
+    public class WordsService
+    {
+        private readonly TrainerContext _context;
+        private readonly IMapper _mapper;
+
+        public WordsService(TrainerContext context, IMapper mapper)
+        {
+            _mapper = mapper;
+            _context = context;
+        }
+
+        public async Task<WordDTO> GetWord(int id)
+        {
+            var word = await _context.Words.FirstOrDefaultAsync(w => w.Id == id);
+            return _mapper.Map<WordDTO>(word);
+        }
+
+        public async Task<ICollection<Word>> GetWords()
+        {
+            var words = await _context.Words.ToListAsync();
+            return _mapper.Map<ICollection<Word>>(words);
+        }
+    }
+}
---
+++ backend/Trainer/Trainer.Common/DTO/WordDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Trainer.Common.DTO
+{
+    public sealed class WordDTO
+    {
+        public string Text { get; set; }
+        public string Transcription { get; set; }
+        public string Translation { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+    }
+}
