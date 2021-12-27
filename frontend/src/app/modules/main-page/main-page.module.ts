15a5bd9ea7a15a9b5526107d47fdf1d966dea813
sleepergoose <sleepergoose@gmail.com>
2021-12-27 19:08:41 +0200

    Created track card component

--- frontend/src/app/modules/main-page/main-page.module.ts
+++ frontend/src/app/modules/main-page/main-page.module.ts
@@ -2,14 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './components/main-page/main-page.component';
 import { MainPageRoutingModule } from './main-page-routing.module';
+import { TrackCardComponent } from './components/track-card/track-card.component';
+import { MaterialComponentsModule } from '../material-components/material-components.module';
 
 @NgModule({
   declarations: [
-    MainPageComponent
+    MainPageComponent,
+    TrackCardComponent
   ],
   imports: [
     CommonModule,
-    MainPageRoutingModule
+    MainPageRoutingModule,
+    MaterialComponentsModule
   ]
 })
 export class MainPageModule { }
