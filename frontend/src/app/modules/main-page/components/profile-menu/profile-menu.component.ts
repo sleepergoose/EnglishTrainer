91a7c48fe7e8f87d5877a9b2fc2f856c05d70dd6
sleepergoose <sleepergoose@gmail.com>
2021-12-29 13:55:21 +0200

    Added profile menu

---
+++ frontend/src/app/modules/main-page/components/profile-menu/profile-menu.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-menu',
+  templateUrl: './profile-menu.component.html',
+  styleUrls: ['./profile-menu.component.sass']
+})
+export class ProfileMenuComponent {
+  constructor() { }
+}
