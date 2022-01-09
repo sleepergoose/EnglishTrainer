7610de6a89a2b12243d571cf514371df0518a194
sleepergoose <sleepergoose@gmail.com>
2022-01-09 14:08:39 +0200

    Renamed main-page module to main; restyled login & register component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
   },
   {
     path: '',
-    loadChildren: () => import('./modules/main-page/main.module').then((m) => m.MainModule),
+    loadChildren: () => import('./modules/main/main.module').then((m) => m.MainModule),
     canActivate: [AuthGuard]
   }
 ];
