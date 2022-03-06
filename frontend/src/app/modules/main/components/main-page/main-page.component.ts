3263e924786705cc8f6160b1c72b7b11d2e6f1e9
sleepergoose <sleepergoose@gmail.com>
2022-03-06 23:08:51 +0200

    Complete the task

--- frontend/src/app/modules/main/components/main-page/main-page.component.ts
+++ frontend/src/app/modules/main/components/main-page/main-page.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { debounceTime, distinctUntilChanged, Subject, switchMap, take, takeUntil } from 'rxjs';
+import { debounceTime, distinctUntilChanged, mergeMap, Subject, switchMap, take, takeUntil } from 'rxjs';
 import { TrackCardRead } from 'src/app/models/track/track-card-read';
 import { WordEdit } from 'src/app/models/word/word-edit';
 import { WordRead } from 'src/app/models/word/word-read';
+import { PhrasalVerbService } from 'src/app/services/phrasal-verb.service';
 import { SearchService } from 'src/app/services/search.service';
 import { WordTrackService } from 'src/app/services/word-track.service';
 import { WordsService } from 'src/app/services/words.service';
@@ -27,7 +28,8 @@ export class MainPageComponent implements OnInit {
   constructor(
     private _wtService: WordTrackService,
     private _searchService: SearchService,
-    private _wordService: WordsService
+    private _wordService: WordsService,
+    private _pvService: PhrasalVerbService
   ) { }
 
   ngOnInit() {
@@ -70,12 +72,51 @@ export class MainPageComponent implements OnInit {
         this.foundWords = data;
       }
     });
+
+    this._searchTerms.pipe(
+      takeUntil(this._unsubscribe$),
+      debounceTime(500),
+      distinctUntilChanged(),
+      switchMap((term: string) => this._searchService.getPVsByName(term)),
+    ).subscribe({
+      next: (data) => {     
+        const verbs = data.map(verb => {
+          return {
+            id: verb.id,
+            text: verb.text,
+            transcription: 'pv',
+            translation: verb.translation
+          } as WordRead;
+        });
+
+        this.foundWords = [...this.foundWords, ...verbs];
+      }
+    });
   }
 
   selectWord(word: string) {  
-    const id = this.foundWords.find(w => w.text === word)!.id;
-    
-    this._wordService.getWordById(id)
+    const item = this.foundWords.find(w => w.text === word)!;
+    this.searchValue = '';
+
+    if (item.transcription === 'pv') {
+      this._pvService.getPhrasalVerbById(item.id)
+      .pipe(take(1))
+      .subscribe((result) => {
+
+        this.word = { 
+          id: result.id,
+          text: result.text,
+          transcription: '',
+          translation: result.translation,
+          examples: result.examples
+        } as WordEdit;
+
+        this.foundWords = new Array<WordRead>();
+        this.showSearchResult = true;
+      });
+    }
+    else {
+      this._wordService.getWordById(item.id)
       .pipe(take(1))
       .subscribe((result) => {
 
@@ -83,5 +124,6 @@ export class MainPageComponent implements OnInit {
         this.foundWords = new Array<WordRead>();
         this.showSearchResult = true;
       });
+    }
   }
 }
