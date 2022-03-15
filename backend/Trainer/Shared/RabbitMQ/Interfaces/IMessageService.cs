f69c92e5ca3f3bd43d6996177e3536887585879a
sleepergoose <sleepergoose@gmail.com>
2022-03-15 15:37:26 +0200

    Added RabbitMQ.Client wrapper

---
+++ backend/Trainer/Shared/RabbitMQ/Interfaces/IMessageService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using RabbitMQ.Client.Events;
+using RabbitMQ.Wrapper.Models;
+using RabbitMQ.Wrapper.Services;
+
+namespace RabbitMQ.Wrapper.Interfaces
+{
+    public interface IMessageService
+    {
+        event MessageService.MessageDelegate MessageReceived;
+
+        void SetMessageService(ScopeSettings producerSettings, ScopeSettings consumerSettings);
+
+        void SendDataToQueue(byte[] body);
+
+        void ListenQueue(Object model, BasicDeliverEventArgs ea);
+
+        void Dispose();
+    }
+}
---
+++ backend/Trainer/Shared/RabbitMQ/Models/ProducerSettings.cs
@@ -0,0 +1,11 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Wrapper.Interfaces;
+
+namespace RabbitMQ.Wrapper.Models
+{
+    internal class ProducerSettings
+    {
+        public IModel Channel { get; set; }
+        public PublicationAddress PublicationAddress { get; set; }
+    }
+}
